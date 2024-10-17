0560f7218846b5505b085f248581fbabf49c3102
dutrazzx <victorfdutraa@gmail.com>
2024-10-17 11:26:06 -0300

    /trabalholedson

---
+++ trabalholedson/script.js
@@ -0,0 +1,19 @@
+html {
+  height: 100 %;
+  width: 100 %;
+}
+
+#alert {
+  font - size: 64px;
+}
+
+.Preto01 {
+  display: flex;
+}
+.Preto02{
+  width: 40vw;
+  height: 40vh;
+  display: flex;
+  justify - content: center;
+  align - items: center;
+}
