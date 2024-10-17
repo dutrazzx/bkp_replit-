ce75d9d3702017c91bc118995bc9213d08a42fa5
dutrazzx <victorfdutraa@gmail.com>
2024-10-17 11:28:09 -0300

    array01

---
+++ Array01/script.js
@@ -0,0 +1,14 @@
+// VARIAVEIS UNITÁRIAS
+const name = 'zequinha';
+const nome2 = 'tirulipa';
+const nome3 = 'kabrinha';
+
+
+
+
+// VARAVEIS DE ARRAY
+const nomes = ['zequinha', 'tirulipa', 'kabrinha'];
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = "juninho pagodinho"
+console.log(nomes[10]) 
